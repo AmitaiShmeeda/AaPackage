29ee9ad5c90806734414d06a82bc0ef63144ed1f
amitai shmeeda <amitaishmeeda@gmail.com>
2021-12-07 22:23:33 +0200

    more func more color

---
+++ app/src/main/java/com/example/aapackage/model/Package.kt
@@ -0,0 +1,13 @@
+package com.example.aapackage.model
+
+data class Package (
+
+        var type_package: String,
+        var fragile: Boolean,
+        var weight: Float,
+        var Recipient_name: String,
+        var Recipient_address: String,
+
+//        TODO location of storage API
+
+    )
