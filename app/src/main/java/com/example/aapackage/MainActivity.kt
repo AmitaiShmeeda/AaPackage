b597d96b56de9914fd832645e593a29c7eb09e91
amitai shmeeda <amitaishmeeda@gmail.com>
2021-12-07 15:46:35 +0200

    first stage- open screen

---
+++ app/src/main/java/com/example/aapackage/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.aapackage
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.aapackage.splashActivity.HomeActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        //splash screen
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        },4000)
+
+        //end splash screen
+    }
+}
---
+++ app/src/main/java/com/example/aapackage/splashActivity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aapackage.splashActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aapackage.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
